9ad3fe439f64ff28d67a647c1d3a88989c46b1ff
RockPigeon <rockpigeon95@gmail.com>
2023-01-24 17:36:29 +0900

    Day 16 coding exercise complete

--- coding exersice 16.py
+++ coding exersice 16.py
@@ -1,15 +1,23 @@
 import PySimpleGUI as sg
 
 feet_label = sg.Text("Enter feet:")
-feet_box = sg.InputText()
+feet_box = sg.InputText(key='feet')
 
 inches_label = sg.Text("Enter inches:")
-inches_box = sg.InputText()
+inches_box = sg.InputText(key='inches')
 
 convert_button = sg.Button("Convert")
+meters_label = sg.Text(key='meters')
 
 window = sg.Window("Imperial to Metric Converter", layout=[[feet_label, feet_box],
                                                            [inches_label, inches_box],
-                                                           [convert_button]])
-window.read()
+                                                           [convert_button, meters_label]])
+while True:
+    event, values = window.read()
+    print(event, values)
+    feet = float(values['feet'])
+    inches = float(values['inches'])
+    meters = (feet * 30.48)/100 + (inches * 2.54)/100
+    window['meters'].update(f"{meters}m")
+
 window.close()
